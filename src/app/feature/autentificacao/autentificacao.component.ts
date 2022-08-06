6e85fdcd6c7d39bef743d24702d547de97267a52
Lais <lais3722@gmail.com>
2022-08-06 20:51:26 -0300

    adicionando pacotes

---
+++ src/app/feature/autentificacao/autentificacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autentificacao',
+  templateUrl: './autentificacao.component.html',
+  styleUrls: ['./autentificacao.component.css']
+})
+export class AutentificacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
