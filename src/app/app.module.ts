99012cd304973ceedf4e441d51d0d4f8b24a27bc
Laís Portela <laisportela@wizsolucoes.com.br>
2022-08-06 20:51:26 -0300

    adicionando pacotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProdutoComponent } from './feature/produto/produto.component';
+import { CompraComponent } from './feature/compra/compra.component';
+import { AutentificacaoComponent } from './core/autentificacao/autentificacao.component';
+import { ServicoComponent } from './core/servico/servico.component';
+import { LoginComponent } from './feature/autentificacao/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    CompraComponent,
+    AutentificacaoComponent,
+    ServicoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
