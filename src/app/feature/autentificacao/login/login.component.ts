4a7a6534b481fd2276556b7b5aa907e8e90acd82
Douglas Monteles <silvadouglas328@gmail.com>
2022-08-08 22:03:33 -0300

    Corrigindo a estrutura das pastas

--- src/app/feature/autentificacao/login/login.component.ts
+++ src/app/feature/autentificacao/login/login.component.ts
@@ -1,4 +1,7 @@
+import { FormService } from '../../../services/form.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+
+  constructor(
+    private _auth: AuthService,
+    private _fb: FormBuilder,
+    public form: FormService,
+  ) {
+    this.formLogin = this._fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  public login(): void {
+    this._auth.login(this.formLogin.value).subscribe({
+      next: (response) => console.log(response),
+    });
   }
 
 }
