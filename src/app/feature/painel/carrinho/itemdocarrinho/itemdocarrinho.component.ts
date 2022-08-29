307008c574e0914ce6222cbcbc515fe205a1816b
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2022-08-29 20:03:31 -0300

    um carrinho muito simples

---
+++ src/app/feature/painel/carrinho/itemdocarrinho/itemdocarrinho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itemdocarrinho',
+  templateUrl: './itemdocarrinho.component.html',
+  styleUrls: ['./itemdocarrinho.component.css']
+})
+export class ItemdocarrinhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
