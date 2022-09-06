db64b8d67f29c4c84c761d74ab4e01a0d9fbe6e2
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2022-09-05 23:04:35 -0300

    corrigindo importações do modulo

--- src/app/feature/painel/painel.component.ts
+++ src/app/feature/painel/painel.component.ts
@@ -7,18 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor() { 
-=======
-  visibilidadeDoCarrinho : boolean
-
-  constructor() { 
-    this.visibilidadeDoCarrinho = true
-  }
-
-  alteraVisibilidadeDoCarrinho(){
-    this.visibilidadeDoCarrinho  = ! this.visibilidadeDoCarrinho
->>>>>>> 67327514c1c3c818aee57de9c48204de3260b609
   }
 
   ngOnInit(): void {
