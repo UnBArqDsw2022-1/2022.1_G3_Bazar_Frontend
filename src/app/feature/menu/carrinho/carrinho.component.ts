edbcfffbbc62ba14231a29bc4906a7b71291e7f2
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2022-09-08 08:34:20 -0300

    adicionando botão comprar

--- src/app/feature/menu/carrinho/carrinho.component.ts
+++ src/app/feature/menu/carrinho/carrinho.component.ts
@@ -2,6 +2,7 @@ import { PedidoService } from './../../../services/pedido.service';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ItemPedido } from 'src/app/models/ItemPedido.model';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { ProdutoComponent } from '../../painel/produto/produto.component';
 
 @Component({
   selector: 'app-carrinho',
@@ -34,6 +35,14 @@ export class CarrinhoComponent implements OnInit {
     this.itens = this._pedidoService.decrementar(produtoId);
   }
 
+  public precoTotal():number{
+    let sum = 0;
+    for (let e of this.itens)
+      sum += e.produto.preco * e.quantidade
+
+    return sum
+  }
+
 
 
   ngOnInit(): void {
@@ -51,6 +60,63 @@ export class CarrinhoComponent implements OnInit {
       {
         produto: {
           id: 2,
+          nome: 'Teste 2',
+          preco: 12.0,
+        },
+
+        quantidade: 4,
+      },
+      {
+        produto: {
+          id: 3,
+          nome: 'Teste',
+          preco: 10.0,
+        },
+
+        quantidade: 2,
+      },
+
+      {
+        produto: {
+          id: 4,
+          nome: 'Teste 2',
+          preco: 12.0,
+        },
+
+        quantidade: 4,
+      },
+      {
+        produto: {
+          id: 5,
+          nome: 'Teste',
+          preco: 10.0,
+        },
+
+        quantidade: 2,
+      },
+
+      {
+        produto: {
+          id: 6,
+          nome: 'Teste 2',
+          preco: 12.0,
+        },
+
+        quantidade: 4,
+      },
+      {
+        produto: {
+          id: 7,
+          nome: 'Teste',
+          preco: 10.0,
+        },
+
+        quantidade: 2,
+      },
+
+      {
+        produto: {
+          id: 8,
           nome: 'Teste 2',
           preco: 12.0,
         },
